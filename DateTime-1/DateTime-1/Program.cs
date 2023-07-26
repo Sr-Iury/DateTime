ed38791c245146b8185c120349e181a38d8a676b
Iury <iury.silvafim@gamil.com>
2023-07-26 09:48:26 -0300

    Segunda Parte

--- DateTime-1/DateTime-1/Program.cs
+++ DateTime-1/DateTime-1/Program.cs
@@ -1,8 +1,32 @@
-﻿public class Program {
+﻿using System.Globalization;
+public class Program
+{
     static void Main(string[] args)
     {
-        DateTime data = new DateTime(2023, 07, 26);//estancia da classe DateTime
-        Console.WriteLine(data.ToString("dd/MM/yyyy"));
+        DateTime data = new DateTime(2023, 07, 26);//estancia da classe DateTime, e passando parametros dos metodos.
+        DateTime data2 = new DateTime(2007, 04, 28);
+        TimeSpan ts = data.Subtract(data2);
+        Console.WriteLine(ts.Days);
+        DateOnly dol = new DateOnly(2023, 07, 26);
+        Console.WriteLine(dol);
+        int difuDatas = DateTime.Compare(data, data2);
+        Console.WriteLine(difuDatas);
+        /*DateTime dataVencimento = data.AddDays(2);
+        DateTime HORArond = Convert.ToDateTime(DateTime.UtcNow);
+        DateTime hora = HORArond.AddHours(-4);*/
+
+        /*Console.WriteLine(data.Year);//To.String Faz por na sua ordem.
+       Console.WriteLine(data.DayOfYear);//To.String Faz por na sua ordem.
+       Console.WriteLine(DateTime.UtcNow.Hour);
+       Console.WriteLine(data.ToString("ddd", new CultureInfo("br-pt")));
+       DateTime HORArond = Convert.ToDateTime(DateTime.UtcNow.Hour);
+       DateTime total = HORAron - 
+
+
+
+      Console.Write("Informe sua data de nascimento: ");
+       DateTime dataNas = DateTime.Parse("1995/08/23");
+       Console.WriteLine(dataNas);*/
     }
 }
 
