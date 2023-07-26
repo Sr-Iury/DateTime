c5ed694c0a59f1db9816e74104e28857b60b1cdd
Iury <iury.silvafim@gamil.com>
2023-07-26 08:48:58 -0300

    Primeira parte

---
+++ DateTime-1/DateTime-1/Program.cs
@@ -0,0 +1,9 @@
+﻿public class Program {
+    static void Main(string[] args)
+    {
+        DateTime data = new DateTime(2023, 07, 26);//estancia da classe DateTime
+        Console.WriteLine(data.ToString("dd/MM/yyyy"));
+    }
+}
+
+
